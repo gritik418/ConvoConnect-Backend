0600896503a89a41d91568a1adbc3ce70d02faaf
Ritik <160255700+gritik418@users.noreply.github.com>
2024-06-16 09:30:47 +0530

    Cookie options updated

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -4,7 +4,9 @@ import { CookieOptions } from "express";
 const cookieOptions: CookieOptions = {
   sameSite: "none",
   httpOnly: true,
+  domain: process.env.CLIENT_URL,
   secure: true,
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
 
 const corsOptions: CorsOptions = {
